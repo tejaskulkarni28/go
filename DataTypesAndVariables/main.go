932690588d3b92b8fb0d3c5d68ffd62b5f3a827c
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-01-10 12:27:05 +0530

    Commited

---
+++ DataTypesAndVariables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*
+		1. Integer
+		2. String
+		3. Float (32, 64)
+		4. Boolean
+	*/
+	var Integer int = 10;
+	var String string = "Hello! String"
+	var Boolean bool = false; 
+
+	fmt.Println("This is the output for Integer: ", Integer)
+	fmt.Println("This is the output for String:  ", String)
+	fmt.Println("This is the output for Boolean: ", Boolean)
+	
+}
