ec9b999c396d197252334b5c77f30f820ba11c43
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-01-10 12:40:09 +0530

    Commited

--- DataTypesAndVariables/main.go
+++ DataTypesAndVariables/main.go
@@ -12,9 +12,13 @@ func main(){
 	var Integer int = 10;
 	var String string = "Hello! String"
 	var Boolean bool = false; 
-
+  	var Float32 float32 = 20.21;
+	var Float64 float64 = 30.31;
+	
 	fmt.Println("This is the output for Integer: ", Integer)
 	fmt.Println("This is the output for String:  ", String)
 	fmt.Println("This is the output for Boolean: ", Boolean)
-	
+   	fmt.Println("This is the output for Float32: ", Float32)
+	fmt.Println("This is the output for Float64: ", Float64)
+
 }
